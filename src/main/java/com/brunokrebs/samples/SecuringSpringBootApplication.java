856cff697d8be12c872e2ad6285a497a44954a73
bruno.krebs <krebs.bruno@gmail.com>
2017-03-30 21:27:05 -0300

    first commit

---
+++ src/main/java/com/brunokrebs/samples/SecuringSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.brunokrebs.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringSpringBootApplication.class, args);
+	}
+}
